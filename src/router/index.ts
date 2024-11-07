989cea3fcaa0a3912c78b6a3cb473690166a3bfe
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-11-07 20:38:11 +0800

    [14] Ad favourite route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import Favourite from "../views/Favourite.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/favorites",
+    name: "Favorites",
+    component: Favourite,
   },
 ];
 
