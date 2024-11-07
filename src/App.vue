13dfa26fa31e134db59c268bed7add7fcee3dd57
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-11-07 17:34:13 +0800

    [10] Create anv bar and display home page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-100">
+    <nav class="bg-white shadow-md">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+          <div class="flex">
+            <router-link to="/" class="flex-shrink-0 flex items-center">
+              <img
+                class="h-8 w-auto"
+                src="../src/assets/recipe.png"
+                alt="Recipe App Logo"
+              />
+            </router-link>
+            <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
+              <router-link
+                to="/"
+                class="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+              >
+                Home
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <main>
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <router-view></router-view>
+      </div>
+    </main>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style scoped></style>
