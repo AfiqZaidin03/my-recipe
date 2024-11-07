fac4a43e5bc9b31a7ec9365c887a92c2e6843efb
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-11-07 17:11:35 +0800

    [6] Create recipe card

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="bg-white shadow-md rounded-lg overflow-hidden cursor-pointer hover:shadow-lg transition-shadow duration-300"
+  >
+    <img
+      :src="recipe.image[0]"
+      :alt="recipe.name"
+      class="w-full h-48 object-cover"
+    />
+    <div class="p-4">
+      <h2 class="text-xl font-semibold mb-2">{{ recipe.name }}</h2>
+      <p class="text-gray-600 mb-2">By {{ recipe.author.name }}</p>
+      <p class="text-gray-500 text-sm">{{ recipe.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { Recipe } from "../types/Recipe";
+
+export default defineComponent({
+  name: "RecipeCard",
+  props: {
+    recipe: {
+      type: Object as PropType<Recipe>,
+      required: true,
+    },
+  },
+});
+</script>
