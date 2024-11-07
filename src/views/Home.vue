7d989bb40ddb73273eed01543ae1abc44b11b725
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-11-07 17:21:33 +0800

    [8] Create home page

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold mb-6">Recipes</h1>
+    <div class="mb-4">
+      <input
+        v-model="searchQuery"
+        type="text"
+        placeholder="Search recipes..."
+        class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed, onMounted } from "vue";
+import RecipeCard from "../components/RecipeCard.vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    RecipeCard,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
