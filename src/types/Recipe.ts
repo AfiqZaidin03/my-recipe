b25b3838b03a315c215f6fbd8620b9f346f1fa44
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-11-07 17:02:01 +0800

    [3] Create recipe interface

---
+++ src/types/Recipe.ts
@@ -0,0 +1,25 @@
+export interface Recipe {
+  name: string;
+  url: string;
+  image: string[];
+  author: Author;
+  datePublished: string;
+  dateModified: string;
+  description: string;
+  prepTime: string;
+  cookTime: string;
+  totalTime: string;
+  keywords: string;
+  recipeCuisine: string;
+  recipeCategory: string;
+  recipeIngredient: string[];
+  recipeInstructions: RecipeInstruction[];
+}
+
+export interface Author {
+  name: string;
+}
+
+export interface RecipeInstruction {
+  text: string;
+}
