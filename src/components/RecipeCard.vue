85cf244f6c56b90cc0443a58bed6bde2c89508ee
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-11-07 17:42:19 +0800

    [12] Edit color, font and add line clamp

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -8,9 +8,9 @@
       class="w-full h-48 object-cover"
     />
     <div class="p-4">
-      <h2 class="text-xl font-semibold mb-2">{{ recipe.name }}</h2>
-      <p class="text-gray-600 mb-2">By {{ recipe.author.name }}</p>
-      <p class="text-gray-500 text-sm">{{ recipe.description }}</p>
+      <h2 class="text-xl font-bold mb-2">{{ recipe.name }}</h2>
+      <p class="text-blue-600 mb-4 font-light">By {{ recipe.author.name }}</p>
+      <p class="text-gray-500 text-sm line-clamp-6">{{ recipe.description }}</p>
     </div>
   </div>
 </template>
