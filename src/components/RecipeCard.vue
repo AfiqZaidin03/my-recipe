1930321b5fea9f1b08ac743802d2367034695dcc
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-11-07 20:39:17 +0800

    [15] Fix is author name is unknown show unknown and add condition if image lenth more than 0

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -3,13 +3,16 @@
     class="bg-white shadow-md rounded-lg overflow-hidden cursor-pointer hover:shadow-lg transition-shadow duration-300"
   >
     <img
+      v-if="recipe.image && recipe.image.length > 0"
       :src="recipe.image[0]"
       :alt="recipe.name"
       class="w-full h-48 object-cover"
     />
     <div class="p-4">
       <h2 class="text-xl font-bold mb-2">{{ recipe.name }}</h2>
-      <p class="text-blue-600 mb-4 font-light">By {{ recipe.author.name }}</p>
+      <p class="text-blue-600 mb-4 font-light">
+        By {{ recipe.author?.name || "Unknown" }}
+      </p>
       <p class="text-gray-500 text-sm line-clamp-6">{{ recipe.description }}</p>
     </div>
   </div>
