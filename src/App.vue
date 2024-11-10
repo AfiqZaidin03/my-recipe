862031d40d4ac5832c004e146f36b1af681f6688
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-11-10 16:40:26 +0800

    [31] Add hover text color and border  to gray, and indigo color when its at the related page. Add circle to img

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,37 @@
         <div class="flex justify-between h-16">
           <div class="flex">
             <router-link to="/" class="flex-shrink-0 flex items-center">
-              <img
-                class="h-8 w-auto"
-                src="../src/assets/recipe.png"
-                alt="Recipe App Logo"
-              />
+              <div
+                class="bg-indigo-500 rounded-full p-2 flex items-center justify-center"
+              >
+                <img
+                  class="h-8 w-auto"
+                  src="../src/assets/recipe.png"
+                  alt="Recipe App Logo"
+                />
+              </div>
             </router-link>
             <div class="mt-4 md:mt-0 ml-4 sm:ml-6 sm:flex sm:space-x-8">
               <router-link
                 to="/"
-                class="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                class="inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2 transition duration-300"
+                :class="{
+                  'border-indigo-500 text-indigo-500': $route.path === '/',
+                  'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-300':
+                    $route.path !== '/',
+                }"
               >
                 Home
               </router-link>
               <router-link
                 to="/favorites"
-                class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                class="inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2 transition duration-300"
+                :class="{
+                  'border-indigo-500 text-indigo-500':
+                    $route.path === '/favorites',
+                  'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-300':
+                    $route.path !== '/favorites',
+                }"
               >
                 Favorites
               </router-link>
